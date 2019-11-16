e619dc20235e2bccc18cd17eb99edfd94aa0dcbb
Mantas Tarutis <mantas.tarutis@gmail.com>
2019-11-16 18:44:15 +0200

    fix

--- carry.py
+++ carry.py
@@ -3,16 +3,16 @@ def carry(a, b):
     temp = 0
     digit_count = max((len(str(a))), (len(str(b))))
     for i in range(digit_count):
-        if a % 10 + b % 10 + temp >= 10:
+        last_a = a % 10
+        last_b = b % 10
+        if last_a + last_b + temp >= 10:
             temp = 1
             count += 1
-            a //= 10
-            b //= 10
-        else:
-            break
+        a //= 10
+        b //= 10
 
     return count
 
 
-carry_count = carry(9, 999989999)
+carry_count = carry(190101, 909909)
 print(carry_count)
